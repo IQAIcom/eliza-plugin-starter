97a0be086bd585ca0ff303e371426b5868ab71dd
Srujan Gurram <srujangs8@gmail.com>
2025-01-09 11:59:33 +0530

    Update tsup config with new entry points and settings

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,15 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: {
 		index: "src/index.ts",
-		client: "src/client.ts",
+		plugins: "src/plugins/*/index.ts",
+		scripts: "src/scripts/*.ts",
 	},
-	format: ["cjs", "esm"],
+	format: ["esm", "cjs"],
 	dts: true,
 	splitting: true,
 	sourcemap: true,
 	clean: true,
-	external: ["react"],
+	outDir: "dist",
+	target: "node18",
+	treeshake: true,
 });
